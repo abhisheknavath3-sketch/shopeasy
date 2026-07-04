1de4901639c8a5d87f4f330b66d0b76903957f24
abhisheknavath3-sketch <abhisheknavath3@gamil.com>
2026-07-04 12:21:37 +0530

    first commit

--- ecommerce/src/pages/Checkout.jsx
+++ ecommerce/src/pages/Checkout.jsx
@@ -25,7 +25,7 @@ const Checkout = () => {
             const token = localStorage.getItem("token");
 
             const response = await fetch(
-                " https://shopeasy-kghk.onrender.com/api/orders",
+                " https://shopeasy-kghk.onrender.com/api/orders/createOrder",
                 {
                     method: "POST",
                     headers: {
